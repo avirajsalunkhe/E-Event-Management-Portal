82de6ee3165e4f43058ddc1e49987eff8b008867
Aviraj Salunkhe <96661930+avirajsalunkhe@users.noreply.github.com>
2024-07-31 01:03:54 +0530

    Add files via upload

---
+++ EventManagementSystems/aboutus.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Sanchanala 2k20</title>
+<?php require 'utils/styles.php'; ?><!--css links. file found in utils folder-->
+</head>
+<style>
+
+/* Large rounded green border */
+hr.blueline {
+  border: 10px solid #00004d;
+  border-radius: 5px;
+}
+
+#bj
+{
+  font-size: 22px;
+}
+</style>
+
+<body style="background-color:#b3e0ff ">
+
+  <?php require 'utils/header.php'; ?>
+  <hr class="blueline">
+  <div class="col-md-12">
+  
+<h1>About Us</h1>
+
+<p id="bj"> SBPCOE collage of engineering, indhapur affiliated to Savitribai Phule Pune University(sppu), vangali, indapur, approved by AICTE New Delhi, DTE, and Government of Maharashtra.was established in the year 2009 by Harshavardhanji Patil  which is a trust formed by a group of well known academicians. This Visionary group is committed to develop SBPCOE as a paradigm within a couple of years. The future of SBPCOE in Engineering Education is bright as the above professors are heading the departments and extend their service to develop the institution with the support of highly qualified and dedicated teaching and non-teaching staff,
+</p>
+
+<p id="bj">Prof DR SHRINIWAS T SHIRKANDE, a well-known academician and administrator with-over 50 years of academic and administrative experience to his credit along with an excellent team of popular professors of SBPCOE.</p>
+
+<p id="bj">Associate Professor, Mrs. Gaikwad Manjushree Vishwasrao&  Mr.Admile Pushkraj Ramkrushna, in association with Mr.Ghogare, a practicing Civil Engineer, MR. GOLE AMOL SANDIPAN renowned Chartered Accountant and MR. GAIKWAD DHIRAJ TUKARAM well known administrator both at SBPCOE. SBPCOE will provide students with a sense of history, an understanding of the ideals and principles, a commitment to law and morality, an appreciation of human creativity an analytically inquisitive mind. We believe that once the students experience these, they are ready to address the challenges of the rapidly transforming world.
+</p>
+<p id="bj">Kalavishkar is a cultural fest which is held every year, we like everyone to participate cheerfully in every event. </p>  
+</div>
+<hr class="blueline">
+</body>
+
+ <?php require 'utils/footer.php'; ?>
+
+</html>
---
+++ EventManagementSystems/utils/footer.php
@@ -0,0 +1,40 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>S</strong><span class = "small footerSubtext">.</span>
+                        <strong>B</strong><span class = "small footerSubtext">.</span>
+                        <strong>P</strong><span class = "small footerSubtext">atil</span>
+                        <strong>C</strong><span class = "small footerSubtext">ollege of</span>
+                        <strong>E</strong><span class = "small footerSubtext">ngineering</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                    Our college Mission is to impart quality technical education and higher moral ethics associated with skilled training to suit the modern day technology with innovative concepts,so as to learn to lead the future with full confidence
+                  .
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                <p> <br>
+                 Address:GAT No: 58, Village – Vangali, Pune-
+Sholapur highway, Indapur 
+                    <br>Pune – 413106 </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                    <a href="https://www.facebook.com/sbpcoeindapur?mibextid=ZbWKwL"><img src = "images/facebook.png"></a>
+                    <a href="https://www.instagram.com/sbpatilgroup?igsh=MWN4ZDNwZzVqaDgzcg=="><img src = "images/instagram.png" width="65px"></a>
+                        
+                        <a href="https://youtube.com/@sbpatileducationalinstitut6746?si=2aRjr5iFfQ-_AvaZ"><img src = "images/youtube.png"></a>
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
